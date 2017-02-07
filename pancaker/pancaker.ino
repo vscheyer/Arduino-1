a7e3884d97511a498cd6783c09deaa9ba2a59d9b
David Whitney <dwhitnee@gmail.com>
2017-02-06 22:37:00 -0800

    Speed doesn't work with onestep()

--- pancaker/pancaker.ino
+++ pancaker/pancaker.ino
@@ -1,13 +1,10 @@
 // -*- mode: c -*-
 /*
-   This is a test sketch for the Adafruit assembled Motor Shield for Arduino v2
-   It won't work with v1.x motor shields! Only for the v2's with built in PWM
-   control
-
-   For use with the Adafruit Motor Shield v2
-   ---->        http://www.adafruit.com/products/1438
+ * Joystick control of two stepper motors
+ *
+ * Driver for the Adafruit Motor Shield v2  http://www.adafruit.com/products/1438
+ * API: https://learn.adafruit.com/adafruit-motor-shield-v2-for-arduino/library-reference#class-adafruit-steppermotor
 */
-
 
 #include <Wire.h>
 #include <Adafruit_MotorShield.h>
@@ -16,10 +13,10 @@
 // Create the motor shield object with the default I2C address
 Adafruit_MotorShield AFMS = Adafruit_MotorShield();
 
-// Connect a stepper motor with 200 steps per revolution (1.8 degrees)
-// to motor port #1 and #2 (M3 and M4)
-Adafruit_StepperMotor *motorX = AFMS.getStepper(200, 1);
-Adafruit_StepperMotor *motorY = AFMS.getStepper(200, 2);
+// Connect stepper motors, our motor does 200 steps per revolution (1.8 degrees)
+// Use motor port #1 and #2 (M3 and M4)
+Adafruit_StepperMotor* motorX = AFMS.getStepper( 200, 1 );
+Adafruit_StepperMotor* motorY = AFMS.getStepper( 200, 2 );
 
 int xPin = 2;
 int yPin = 0;
@@ -28,8 +25,9 @@ void setup() {
     Serial.begin(9600);           // set up Serial library at 9600 bps
     Serial.println("Let's make pancakes!");
 
-    AFMS.begin();  // create with the default frequency 1.6KHz, what does that mean?
-    //AFMS.begin(1000);  // OR with a different frequency, say 1KHz
+    // create motor library with the default frequency of 1600Hz for PWM,
+    // I don't know what effect this has on the motor.
+    AFMS.begin();
 }
 
 void loop() {
@@ -47,24 +45,30 @@ void loop() {
     // stepStyle is
     // SINGLE means single-coil activation,
     // DOUBLE means 2 coils are activated at once (for higher torque) and
-    // INTERLEAVE means that it alternates between single and double to get twice the resolution (but of course its half the speed).
-    // MICROSTEP is a method where the coils are PWM'd to create smooth motion between steps.
+    // INTERLEAVE means that it alternates between single and double to get twice the resolution (but of course it's half the speed).
+    // MICROSTEP is a slow method where the coils are PWM'd to create smooth motion between steps.
 
-    int stepStyle = DOUBLE;
-    int fast = 800;  // steps/min of motor
-    int slow = 100;
+    int stepStyle = DOUBLE;    // DOUBLE seems to be best combination of torque and speed.
     bool motorRunning = false;
-    
-    motorX->setSpeed( slow );
-    motorY->setSpeed( slow );
+
+/*
+    int fast = 400;  // steps/revolution of motor
+    int slow = 50;
+
+    int xSpeed = slow;
+    int ySpeed = slow;
 
     // make motor move faster if joystick is pushed hard over
     if ((x > 1000) || (x < 10)) {
-        motorX->setSpeed( fast );
+        xSpeed = fast;
     }
     if ((y > 1000) || (y < 10)) {
-        motorY->setSpeed( fast );
+        ySpeed = fast;
     }
+
+    motorX->setSpeed( xSpeed );   // setSpeed is ignored is onestep() is used.
+    motorY->setSpeed( ySpeed );
+*/
 
     if (x > 750) {
         motorX->onestep( FORWARD, stepStyle );
@@ -88,18 +92,5 @@ void loop() {
         motorX->release();   // let motor spin freely (don't lock gear in place)
         motorY->release();   // keeps motor cooler when idle, too
     }
-    
-    /*
-    Serial.println("Double coil steps");
-    motor->step(100, FORWARD, DOUBLE);
-    motor->step(100, BACKWARD, DOUBLE);
 
-    Serial.println("Interleave coil steps");
-    motor->step(100, FORWARD, INTERLEAVE);
-    motor->step(100, BACKWARD, INTERLEAVE);
-
-    Serial.println("Microstep steps");
-    motor->step(50, FORWARD, MICROSTEP);
-    motor->step(50, BACKWARD, MICROSTEP);
-    */
 }
